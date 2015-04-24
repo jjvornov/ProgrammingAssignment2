dedb64902ad90c57b09071021ce4ed45ddc0d49f
jjvornov <jjvornov@mac.com>
2015-04-24 12:07:12 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+##need a solvable matrix like this
+##a<- matrix( c(3,5,9,14), 2, 2)
 
+##If you run x<-makeCacheMatrix(a) then x$set(y) puts y, a vector into a hidden x
+##x$get() returns that hidden x, x$setmean and x$getmean does the sam for a hidden m
 makeCacheMatrix <- function(x = matrix()) {
+m<-NULL
+set<- function (y) {
+x<<- y
+m<<-NULL
+}
+get<- function() x
+setinverse <- function(solve) m<<-solve
+getinverse <- function() m
+list(set = set, get = get, 
+  setinverse =setinverse, 
+  getinverse = getinverse)
 
 }
 
-
-## Write a short comment describing this function
-
+##cacheSolve(x) runs the functions to get the hidden m if it has been cached for the 
+##   variable passed to it.
+##   else it puts the mean of x (the vector passed to it) into the hidden m to initialize
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
